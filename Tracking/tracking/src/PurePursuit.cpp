15d0be546f8451c3656c9d22dfa134ed02cf6fa3
Bang Sangwoo <sangwoo6999@naver.com>
2024-02-29 08:41:45 -0500

    0229.v02

--- Tracking/tracking/src/PurePursuit.cpp
+++ Tracking/tracking/src/PurePursuit.cpp
@@ -12,24 +12,25 @@
 #include "cpprobotics_types.h"
 #include <morai_msgs/CtrlCmd.h>
 #include <morai_msgs/EgoVehicleStatus.h>
-#define DT 0.1
+#define DT 0.02
 #define L 0.5
 #define KP 1.0
-#define MAX_STEER 45.0/180*M_PI
+#define MAX_STEER 20.0/180*M_PI
 
 using namespace std;
 using namespace cpprobotics;
+
 
 ros::Subscriber sub; 
 morai_msgs::CtrlCmd control;
 ros::Publisher pub;
 
-ifstream Out("/home/autonav/catkin_ws/src/morai_tutorial/src/map/morai_highway.txt");
-vector<vector<float>> Map_data(7896,vector<float>(2,0));
+ifstream Out("/home/autonav/catkin_ws/src/morai_tutorial/src/map/morai_highway2.txt");
+vector<vector<float>> Map_data(790,vector<float>(2,0));
 vector<float> Way_point(2,0);
 vector<double> My_enu(3,0);
-vector<float> xx(7896,0);
-vector<float> yy(7896,0);
+vector<float> xx(790,0);
+vector<float> yy(790,0);
 float target_speed = 30.0 / 3.6;
 Vec_f speed_profile;
 Vec_f r_x;
@@ -40,6 +41,7 @@ Vec_f rs;
 int LD = 15;  //lookaheaddistance
 float delta;  // steering angle
 
+int ind = 0;
 //LQR
 State state(-0.0, -0.0, 0.0, 0.0);
 
@@ -63,23 +65,19 @@ Vec_f calc_speed_profile(Vec_f rx, Vec_f ry, Vec_f ryaw, float target_speed){
   return speed_profile;
 };
 
-
-float calc_nearest_index(State state, Vec_f cx, Vec_f cy, Vec_f cyaw, int &ind){
+float calc_nearest_index(State state, Vec_f cx, Vec_f cy, Vec_f cyaw){
   float mind = std::numeric_limits<float>::max();
-  for(unsigned int i=0; i<cx.size(); i++){
+  for(uint32_t i=ind; i<ind + 50; i++){
     float idx = cx[i] - state.x;
     float idy = cy[i] - state.y;
-    float d_e = idx*idx + idy*idy;
+    float d_e = sqrt(idx*idx + idy*idy);
 
     if (d_e<mind){
       mind = d_e;
       ind = i;
     }
   }
-  float dxl = cx[ind] - state.x;
-  float dyl = cy[ind] - state.y;
-  float angle = YAW_P2P(cyaw[ind] - std::atan2(dyl, dxl));
-  if (angle < 0) mind = mind * -1;
+  // if (angle < 0) mind = mind * -1;
 
   return mind;
 };
@@ -107,9 +105,8 @@ Eigen::RowVector4f dlqr(Eigen::Matrix4f A, Eigen::Vector4f B, Eigen::Matrix4f Q,
   return K;
 };
 
-float lqr_steering_control(State state, Vec_f cx, Vec_f cy, Vec_f cyaw, Vec_f ck, int& ind, float& pe, float& pth_e){
-  float e = calc_nearest_index(state, cx, cy, cyaw, ind);
-
+float lqr_steering_control(State state, Vec_f cx, Vec_f cy, Vec_f cyaw, Vec_f ck, float& pe, float& pth_e){
+  float e = calc_nearest_index(state, cx, cy, cyaw);
   float k = ck[ind];
   float th_e = YAW_P2P(state.yaw - cyaw[ind]);
 
@@ -145,12 +142,20 @@ float lqr_steering_control(State state, Vec_f cx, Vec_f cy, Vec_f cyaw, Vec_f ck
 };
 
 
-void closed_loop_prediction(Vec_f cx, Vec_f cy, Vec_f cyaw, Vec_f ck, Vec_f speed_profile){
+void closed_loop_prediction(Vec_f cx, Vec_f cy, Vec_f cyaw, Vec_f ck){
   float e = 0;
   float e_th = 0;
-  int ind =0;
-  delta = lqr_steering_control(state, cx, cy, cyaw, ck, ind, e, e_th);
-  float ai = KP * (speed_profile[ind]-state.v);
+  delta = lqr_steering_control(state, cx, cy, cyaw, ck, e, e_th);
+  if( delta < -1 * MAX_STEER)
+  {
+    delta = -1 * MAX_STEER;
+  }
+  else if(delta > MAX_STEER)
+  {
+    delta = MAX_STEER;
+  }
+  cout << "니어 인덱스: "<<ind<<endl;
+  cout << "니어 디스트: "<<e<<endl;
 };
 
 
@@ -159,7 +164,7 @@ void Morai_pub()
     control.longlCmdType = 2;  //what is it?
 	control.accel = 1;
 	control.brake = 0;
-	control.steering = -delta;
+	control.steering = delta;
 	control.velocity = 30;
 	pub.publish(control);
     
@@ -173,9 +178,9 @@ void callback(const morai_msgs::EgoVehicleStatus::ConstPtr &msg)
     My_enu.emplace_back(msg->heading);
     state.x = My_enu[0];
     state.y = My_enu[1];
-    state.yaw = My_enu[2];
+    state.yaw = My_enu[2] * M_PI / 180;
     state.v = 30;
-    closed_loop_prediction(r_x, r_y, ryaw, rk, speed_profile);
+    closed_loop_prediction(r_x, r_y, ryaw, rk);
     Morai_pub();
 
 
@@ -183,7 +188,7 @@ void callback(const morai_msgs::EgoVehicleStatus::ConstPtr &msg)
 
 void Load_Map() // pick
 {
-	for (int i = 0; i < 7896; i++)
+	for (int i = 0; i < 790; i++)
 	{
 		for (int j = 0; j < 2; j++)
 		{
@@ -205,7 +210,7 @@ int main(int argc, char **argv)
     ros::init(argc,argv,"lqr");
     Load_Map();
     Spline2D csp_obj(xx, yy);
-    for(float i=0; i<csp_obj.s.back(); i+=0.1)
+    for(float i=0; i<csp_obj.s.back(); i+=3)
     {
         array<float, 2> point_ = csp_obj.calc_postion(i);
         r_x.push_back(point_[0]);
@@ -214,9 +219,14 @@ int main(int argc, char **argv)
         rk.push_back(csp_obj.calc_curvature(i));
         rs.push_back(i);
     }
-    speed_profile = calc_speed_profile(r_x, r_y, ryaw, target_speed);
+
+    // for(int i= 0 ; i < r_x.size(); i++)
+    // {
+    //   cout<<"("<<r_x[i]<<","<<r_y[i]<<")"<<endl;
+    // }
+    // speed_profile = calc_speed_profile(r_x, r_y, ryaw, target_speed);
     ros::NodeHandle nh;
-    sub = nh.subscribe<morai_msgs::EgoVehicleStatus>("/Ego_topic",1,callback);
-    pub = nh.advertise<morai_msgs::CtrlCmd>("ctrl_cmd_0",1);
+    sub = nh.subscribe<morai_msgs::EgoVehicleStatus>("/Ego_topic",100,callback);
+    pub = nh.advertise<morai_msgs::CtrlCmd>("ctrl_cmd_0",100);
     ros::spin();
 }
